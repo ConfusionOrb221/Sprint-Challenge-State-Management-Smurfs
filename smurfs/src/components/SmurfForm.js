d2f9e9867887400374842f0b36d9885c57948140
ConfusionOrb221 <nate1898@gmail.com>
2020-02-07 22:46:17 -0500

    Finished Stretch Goals

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {pushSmurf, getSmurfs} from '../actions/Action';
+import {pushSmurf, getSmurfs, delteSmurf, editSmurf} from '../actions/Action';
 
 const NAME = 'NAME';
 const HEIGHT = 'HEIGHT';
 const AGE = 'AGE';
-
+const ID = 'ID';
 
 class SmurfForm extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            name: '',
-            height: '',
-            age: ''
+            new: { 
+                name: '',
+                height: '',
+                age: ''
+            },
+            id: null 
         };
+    }
+
+    editExisting(id){
+        let edited = {};
+        if(this.state.new.name != ''){
+            edited.name = this.state.new.name;
+        }
+        if(this.state.new.height != ''){
+            edited.height = this.state.new.height;
+        }
+        if(this.state.new.age != ''){
+            edited.age = this.state.new.age;
+        }
+        this.setState({id: null, new: this.props.editSmurf(id, edited)});
+    }
+
+    deleteExisintg(id){
+        this.setState({id: null, new: this.props.delteSmurf(id)});
     }
 
     handleSubmit(event){
         event.preventDefault();
-        this.setState(this.props.pushSmurf(this.state));
+        this.setState({id: null, new: this.props.pushSmurf(this.state.new)});
     }
 
     handleChange(input, action){
@@ -28,18 +49,34 @@ class SmurfForm extends Component {
             case NAME:
                 return {
                     ...this.state,
-                    name: input
+                    new: {
+                        ...this.state.new,
+                        name: input
+                    }
                 };
             case HEIGHT:
                 return {
                     ...this.state,
-                    height: input
+                    new: {
+                        ...this.state.new,
+                        height: input
+                    }
                 };
             case AGE:
                 return {
                     ...this.state,
-                    age: input
+                    new: {
+                        ...this.state.new,
+                        age: input
+                    }
                 };
+            case ID:
+                return {
+                    id: input,
+                    new: {
+                        ...this.state.new
+                    }
+                }
             default:
                 return this.state;
         }
@@ -47,24 +84,34 @@ class SmurfForm extends Component {
 
     render() {
         return (
-            <form onSubmit={event => this.handleSubmit(event)}>
-                Name: <input
-                    type="text"
-                    value={this.state.name}
-                    onChange={ref => this.setState(this.handleChange(ref.target.value, NAME))}
-                />
-                Height: <input
-                    type="text"
-                    value={this.state.height}
-                    onChange={ref => this.setState(this.handleChange(ref.target.value, HEIGHT))}
-                />
-                Age: <input
-                    type="text"
-                    value={this.state.age}
-                    onChange={ref => this.setState(this.handleChange(ref.target.value, AGE))}
-                />
-                <input type="submit" value="Submit"/>
-            </form>
+            <div>
+                <h1>Submit New Smurf</h1>
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    Name: <input
+                        type="text"
+                        value={this.state.new.name}
+                        onChange={ref => this.setState(this.handleChange(ref.target.value, NAME))}
+                    />
+                    Height: <input
+                        type="text"
+                        value={this.state.new.height}
+                        onChange={ref => this.setState(this.handleChange(ref.target.value, HEIGHT))}
+                    />
+                    Age: <input
+                        type="text"
+                        value={this.state.new.age}
+                        onChange={ref => this.setState(this.handleChange(ref.target.value, AGE))}
+                    />
+                    <input type="submit" value="Submit"/>
+                </form>
+                ID: <input
+                        type="text"
+                        value={this.state.id}
+                        onChange={ref => this.setState(this.handleChange(ref.target.value, ID))}
+                    />
+                    <button onClick={() => this.editExisting(this.state.id)}> Edit Exiting </button>
+                    <button onClick={() => this.deleteExisintg(this.state.id)}> Delete Exisintg </button>
+            </div>
         );
     }
 }
@@ -75,4 +122,4 @@ const mapStateToProps = state => ({
     error: state.error
 });
 
-export default connect(mapStateToProps, {pushSmurf, getSmurfs}) (SmurfForm);
+export default connect(mapStateToProps, {pushSmurf, getSmurfs, delteSmurf, editSmurf}) (SmurfForm);
